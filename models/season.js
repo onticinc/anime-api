015a73cc608ec7b642c13c7df880c23ab742f8b3
onticinc <onticinc@gmail.com>
2021-12-23 14:57:34 -0700

    debugging mongo

---
+++ models/season.js
@@ -0,0 +1,15 @@
+// our first collection schema
+// season schema
+
+const mongoose = require("mongoose");
+
+const seasonSchema = new mongoose.Schema({
+    url: String, 
+    title: { type: String, required: true },
+    image_url: String, 
+    synopsis: String,
+});
+
+const Season = mongoose.model("Season", seasonSchema);
+
+module.exports = Season;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { Season } = require('./models');
+const app = express();
+
+app.use(express.json());
+app.get("/seasons", async (request, response) => {
+    try{
+        const seasonArray = await Season.find({});
+        response.json({ seasonArray});
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is running at port 3000');
+})
